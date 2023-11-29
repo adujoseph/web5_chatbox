40140123c612ee6f8f695aa91e61669c39cfcc68
adujoseph <iamjosephadu@gmail.com>
2023-11-29 14:23:50 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,152 +15,35 @@ export default function Home() {
 
 
   useEffect(() => {
-    const initWeb5 = async () => {
-      const { web5, did } = await Web5.connect({ sync: '5s' });
-      setWeb5(web5);
-      setMyDid(did);
-      if (web5 && did) {
-        await configureProtocol(web5, did);
-      }
-    };
-    initWeb5();
+
   }, []);
 
 
   const queryLocalProtocol = async (web5) => {
-    return await web5.dwn.protocols.query({
-      message: {
-        filter: {
-          protocol: "https://blackgirlbytes.dev/burn-book-finale",
-        },
-      },
-    });
   };
 
 
   const queryRemoteProtocol = async (web5, did) => {
-    return await web5.dwn.protocols.query({
-      from: did,
-      message: {
-        filter: {
-          protocol: "https://blackgirlbytes.dev/burn-book-finale",
-        },
-      },
-    });
   };
 
   const installLocalProtocol = async (web5, protocolDefinition) => {
-    return await web5.dwn.protocols.configure({
-      message: {
-        definition: protocolDefinition,
-      },
-    });
   };
 
   const installRemoteProtocol = async (web5, did, protocolDefinition) => {
-    return await web5.dwn.protocols.configure({
-      from: did,
-      message: {
-        definition: protocolDefinition,
-      },
-    });
   };
 
   const defineNewProtocol = () => {
-    return {
-      protocol: "https://blackgirlbytes.dev/burn-book-finale",
-      published: true,
-      types: {
-        secretMessage: {
-          schema: "https://example.com/secretMessageSchema",
-          dataFormats: ["application/json"],
-        },
-        directMessage: {
-          schema: "https://example.com/directMessageSchema",
-          dataFormats: ["application/json"],
-        },
-      },
-      structure: {
-        secretMessage: {
-          $actions: [
-            { who: "anyone", can: "write" },
-            { who: "author", of: "secretMessage", can: "read" },
-          ],
-        },
-        directMessage: {
-          $actions: [
-            { who: "author", of: "directMessage", can: "read" },
-            { who: "recipient", of: "directMessage", can: "read" },
-            { who: "anyone", can: "write" },
-          ],
-        },
-      },
-    };
   };
 
 
   const configureProtocol = async (web5, did) => {
-    const protocolDefinition = defineNewProtocol();
-    const protocolUrl = protocolDefinition.protocol;
-
-    const { protocols: localProtocols, status: localProtocolStatus } = await queryLocalProtocol(web5, protocolUrl);
-    if (localProtocolStatus.code !== 200 || localProtocols.length === 0) {
-      const result = await installLocalProtocol(web5, protocolDefinition);
-    }
-
-    const { protocols: remoteProtocols, status: remoteProtocolStatus } = await queryRemoteProtocol(web5, did, protocolUrl);
-    if (remoteProtocolStatus.code !== 200 || remoteProtocols.length === 0) {
-      const result = await installRemoteProtocol(web5, did, protocolDefinition);
-    }
   };
 
 
   const writeToDwnSecretMessage = async (messageObj) => {
-    try {
-      const secretMessageProtocol = defineNewProtocol();
-      const { record, status } = await web5.dwn.records.write({
-        data: messageObj,
-        message: {
-          protocol: secretMessageProtocol.protocol,
-          protocolPath: "secretMessage",
-          schema: secretMessageProtocol.types.secretMessage.schema,
-          recipient: myDid,
-        },
-      });
-
-      if (status.code === 200) {
-        return { ...messageObj, recordId: record.id };
-      }
-
-      console.log('Secret message written to DWN', { record, status });
-      return record;
-    } catch (error) {
-      console.error('Error writing secret message to DWN', error);
-    }
   };
+
   const writeToDwnDirectMessage = async (messageObj) => {
-    try {
-      const directMessageProtocol = defineNewProtocol();
-      const { record, status } = await web5.dwn.records.write({
-        data: messageObj,
-        message: {
-          protocol: directMessageProtocol.protocol,
-          protocolPath: "directMessage",
-          schema: directMessageProtocol.types.directMessage.schema,
-          recipient: messageObj.recipientDid,
-        },
-      });
-
-      if (status.code === 200) {
-        return { ...messageObj, recordId: record.id };
-      }
-
-
-      console.log('Direct message written to DWN', { record, status });
-      return record;
-    } catch (error) {
-      console.error('Error writing direct message to DWN', error);
-    } 
   };
 
   const handleSubmit = async (event) => {
@@ -200,95 +83,17 @@ export default function Home() {
   };
 
   const constructDirectMessage = (recipientDid) => {
-    const currentDate = new Date().toLocaleDateString();
-    const currentTime = new Date().toLocaleTimeString();
 
-    return {
-      text: message,
-      timestamp: `${currentDate} ${currentTime}`,
-      sender: myDid,
-      type: 'Direct',
-      recipientDid: recipientDid,
-      imageUrl: imageUrl,
-    };
   };
 
   const constructSecretMessage = () => {
-    const currentDate = new Date().toLocaleDateString();
-    const currentTime = new Date().toLocaleTimeString();
 
-    return {
-      text: message,
-      timestamp: `${currentDate} ${currentTime}`,
-      sender: myDid,
-      type: 'Secret',
-      imageUrl: imageUrl,
-    };
   };
 
   const fetchUserMessages = async () => {
-    console.log('Fetching sent messages...');
-    try {
-      const response = await web5.dwn.records.query({
-        from: myDid,
-        message: {
-          filter: {
-            protocol: "https://blackgirlbytes.dev/burn-book-finale",
-            schema: "https://example.com/directMessageSchema",
-          },
-        },
-      });
-
-      if (response.status.code === 200) {
-        const userMessages = await Promise.all(
-          response.records.map(async (record) => {
-            const data = await record.data.json();
-            return {
-              ...data,
-              recordId: record.id
-            };
-          })
-        );
-        return userMessages
-      } else {
-        console.error('Error fetching sent messages:', response.status);
-        return [];
-      }
-
-    } catch (error) {
-      console.error('Error in fetchSentMessages:', error);
-    }
   };
 
   const fetchDirectMessages = async () => {
-    console.log('Fetching received direct messages...');
-    try {
-      const response = await web5.dwn.records.query({
-        message: {
-          filter: {
-            protocol: "https://blackgirlbytes.dev/burn-book-finale",
-          },
-        },
-      });
-
-      if (response.status.code === 200) {
-        const directMessages = await Promise.all(
-          response.records.map(async (record) => {
-            const data = await record.data.json();
-            return {
-              ...data,
-              recordId: record.id
-            };
-          })
-        );
-        return directMessages
-      } else {
-        console.error('Error fetching sent messages:', response.status);
-        return [];
-      }
-    } catch (error) {
-      console.error('Error in fetchReceivedDirectMessages:', error);
-    }
   };
 
   const fetchMessages = async () => {
@@ -300,45 +105,9 @@ export default function Home() {
 
 
   const handleCopyDid = async () => {
-    if (myDid) {
-      try {
-        await navigator.clipboard.writeText(myDid);
-        setDidCopied(true);
-        setTimeout(() => {
-          setDidCopied(false);
-        }, 3000);
-      } catch (err) {
-        console.error("Failed to copy DID: " + err);
-      }
-    }
   };
 
   const deleteMessage = async (recordId) => {
-    try {
-      const response = await web5.dwn.records.query({
-        message: {
-          filter: {
-            recordId: recordId,
-          },
-        },
-      });
-
-      if (response.records && response.records.length > 0) {
-        const record = response.records[0];
-        const deleteResult = await record.delete();
-
-        if (deleteResult.status.code === 202) {
-          console.log('Message deleted successfully');
-          setMessages(prevMessages => prevMessages.filter(message => message.recordId !== recordId));
-        } else {
-          console.error('Error deleting message:', deleteResult.status);
-        }
-      } else {
-        console.error('No record found with the specified ID');
-      }
-    } catch (error) {
-      console.error('Error in deleteMessage:', error);
-    }
   };
 
 
